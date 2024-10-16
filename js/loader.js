a29b007d546cf1767481ec8b155459ce05280015
academics700 <139190749+academics700@users.noreply.github.com>
2024-10-16 17:29:17 +0530

    Add files via upload

---
+++ js/loader.js
@@ -0,0 +1,6 @@
+
+window.addEventListener('load', function() {
+    const loader = document.querySelector('.loader-container');
+    loader.style.display = 'none';
+    document.querySelector('.frame-row').style.display = 'flex';
+});
